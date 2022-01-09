d5c0a5640c1fdbdb6dd5ed1df8fb9629606d0c7d
Pavel <prostobux@yandex.ru>
2022-01-09 11:46:22 +0100

    Product details

--- product/views.py
+++ product/views.py
@@ -15,3 +15,15 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise 
+    
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
