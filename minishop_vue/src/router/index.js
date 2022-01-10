b3493f71ae8f5e2efc17e92508da83773b4e6152
Pavel <prostobux@yandex.ru>
2022-01-10 09:45:01 +0100

    Cart created

--- minishop_vue/src/router/index.js
+++ minishop_vue/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   },
   {
     path: '/search',
